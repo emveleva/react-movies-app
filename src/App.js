4c5d3b704fe1e18056194538cbd7412c8890287f
Martina Veleva <m.veleva93@gmail.com>
2021-03-17 19:36:45 +0200

    created html/css 404 page

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 import Register from './components/Register'
 import Login from './components/Login'
+import NotFound from './components/NotFound'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { Route, Switch } from 'react-router-dom'
 import './App.css';
+
 
 function App() {
   return (
@@ -14,6 +16,7 @@ function App() {
         <Switch>
         <Route path='/register' component={Register} />
         <Route path='/login' component={Login} />
+        <Route component={NotFound} />
         </Switch>
         </Router>
       
---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,14 @@
+import style from './NotFound.module.css';
+
+function NotFound() {
+    return (
+      <div className={style.notFound}>
+          <img src="/img/404.png" alt="" srcset=""/>
+          <h1>The page you are trying to reach </h1>
+          <h1> has not been found!</h1>
+      </div>
+    );
+  }
+
+
+export default NotFound
