73708e5a409f9b732f4dc171fe6b82df5fef04c5
Martina Veleva <m.veleva93@gmail.com>
2021-03-19 02:18:23 +0200

    improved and removed some repeating css

--- client/src/components/Profile/Profile.js
+++ client/src/components/Profile/Profile.js
@@ -4,21 +4,16 @@ import style from './Profile.module.css';
 function Profile() {
     return (
       <main className={style.profile}>
-          <li className={style.profileLeft}>
           <h1>Movies to watch</h1>
+          <h1>Already watched</h1>
+          
             <div>
             <img src="/img/to-watch2.png" alt="" srcset=""/>
             </div>
 
-          </li>
-
-          <li className={style.profileRight}>
-          <h1>Already watched</h1>
           <div>
             <img src="/img/watched.png" alt="" srcset=""/>
           </div>
-
-          </li>
       </main>
 
     );
