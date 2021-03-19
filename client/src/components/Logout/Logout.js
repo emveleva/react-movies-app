a26d9e3debae032680448420ddc2195d5fc6c3c2
Martina Veleva <m.veleva93@gmail.com>
2021-03-19 03:30:59 +0200

    added class component

--- client/src/components/Logout/Logout.js
+++ client/src/components/Logout/Logout.js
@@ -1,13 +1,28 @@
 import style from './Logout.module.css';
+import { Component } from 'react';
 
-function Logout() {
-    return (
-      <div className={style.logout}>
-          <img src="/img/logout.png" alt="" srcset=""/>
-          <h1>Thank you for visiting our website!</h1>
-      </div>
-    );
-  }
-    
+// function Logout() {
+//     return (
+//       <div className={style.logout}>
+//           <img src="/img/logout.png" alt="" srcset=""/>
+//           <h1>Thank you for visiting our website!</h1>
+//       </div>
+//     );
+//   }
+     
+
+
+
+class Logout extends Component {
+      render() {
+        return (
+          <div className={style.logout}>
+              <img src="/img/logout.png" alt="" srcset=""/>
+              <h1>Thank you for visiting our website!</h1>
+          </div>
+        );
+      }
+    }
+
     export default Logout
 
